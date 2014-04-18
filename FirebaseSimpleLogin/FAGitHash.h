4e4a8ad03ea6a2fe3c20a67da913569bb5e43d99
Rob DiMarco <rob@firebase.com>
2014-04-18 10:30:50 -0700

    Update Git hash

--- FirebaseSimpleLogin/FAGitHash.h
+++ FirebaseSimpleLogin/FAGitHash.h
@@ -1,6 +1,6 @@
 
 #ifndef FirebaseSimpleLogin_FGitHash_h
 #define FirebaseSimpleLogin_FGitHash_h
-#define kFirebaseSimpleLoginGitHash @"730edef6dbaf45281d0972ff47e2cb7f9e2c6586"
-#define kFirebaseSimpleLoginBuildDate @"20140403"
+#define kFirebaseSimpleLoginGitHash @"c7bc317ded934dfc9b00d9b3e076054e1baa96b3"
+#define kFirebaseSimpleLoginBuildDate @"20140418"
 #endif
