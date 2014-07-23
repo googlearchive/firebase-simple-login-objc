a1cdb9864b7266b3f4e91e4d5b197cb5855a36ef
Katherine Fang <kat@firebase.com>
2014-07-23 14:04:40 -0700

    Removing FAGitHash.h from repo, also adding more things to ignore from git

--- FirebaseSimpleLogin/FAGitHash.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef FirebaseSimpleLogin_FGitHash_h
-#define FirebaseSimpleLogin_FGitHash_h
-#define kFirebaseSimpleLoginGitHash @"4d7230d3e1f16c85231932918a1e399f46df4bee"
-#define kFirebaseSimpleLoginBuildDate @"20140723"
-#endif
